2ad23fb748e5484321c7898c51dd20ea5fdd7cb5
Sean Murphy <sean@iamseanmurphy.com>
2025-01-06 19:17:40 +0000

    5.39

--- antihacker/antihacker.php
+++ antihacker/antihacker.php
@@ -2,7 +2,7 @@
 Plugin Name: AntiHacker 
 Plugin URI: http://antihackerplugin.com
 Description: Improve security, prevent unauthorized access by restrict access to login to whitelisted IP, Firewall, Scanner and more.
-version: 5.38
+version: 5.39
 Text Domain: antihacker
 Domain Path: /language
 Author: Bill Minozzi
